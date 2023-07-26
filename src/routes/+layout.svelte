99a262701a47ec343fb84cff15acdec277e7185b
Darric Heng <darricheng@gmail.com>
2023-07-26 21:59:07 +0800

    add app shell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
+  // Skeleton imports
   // The ordering of these imports is critical to your app working properly
   import '@skeletonlabs/skeleton/themes/theme-modern.css';
   // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
   import '@skeletonlabs/skeleton/styles/skeleton.css';
   // Most of your app wide CSS should be put in this file
   import '../app.postcss';
+
+  // Other imports
+  import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+  <svelte:fragment slot="header">
+    <AppBar>Hello World</AppBar>
+  </svelte:fragment>
+  <slot />
+</AppShell>
