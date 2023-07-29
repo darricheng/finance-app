aa95c6ab2089a3592ec3a9d3d59521d81606b3d2
Darric Heng <darricheng@gmail.com>
2023-07-29 20:27:15 +0800

    Add remaining pages

---
+++ src/routes/income/+page.svelte
@@ -0,0 +1,12 @@
+<div class="p-8 h-full">
+  <div class="grid grid-cols-4 mb-8">
+    <h1 class="h1 col-span-3">Income</h1>
+    <div class="flex flex-row-reverse">
+      <select class="select">
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+      </select>
+    </div>
+  </div>
+  <div class="border-white border-2 rounded-lg h-5/6 w-full">Chart here</div>
+</div>
---
+++ src/routes/monthly-view/+page.svelte
@@ -0,0 +1,26 @@
+<div class="grid grid-cols-4 h-full">
+  <div class="p-4 flex flex-col gap-4 items-center h-full">
+    <h1 class="h1 text-center">Monthly View</h1>
+    <select class="select">
+      <option value="1">Option 1</option>
+      <option value="2">Option 2</option>
+    </select>
+    <div class="border-white border-2 rounded-lg p-4 w-full grow">
+      <div class="flex justify-between">
+        <span>Income</span>
+        <span>5000</span>
+      </div>
+      <div class="flex justify-between">
+        <span>Expenses</span>
+        <span>5000</span>
+      </div>
+      <div class="flex justify-between">
+        <span>Balance</span>
+        <span>5000</span>
+      </div>
+    </div>
+  </div>
+  <div class="p-4 col-span-3">
+    <div class="border-white border-2 rounded-lg h-full w-full">Chart here</div>
+  </div>
+</div>
