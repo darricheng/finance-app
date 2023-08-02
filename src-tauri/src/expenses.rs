c4cd4e8edd8b2d48a51c33f6bd716f91c0fcaced
Darric Heng <darricheng@gmail.com>
2023-08-03 07:44:50 +0800

    Refeactor and cleanup code, add comments

--- src-tauri/src/expenses.rs
+++ src-tauri/src/expenses.rs
@@ -5,6 +5,7 @@ use std::error::Error;
 
 pub struct Expenses {}
 
+/// Intermediate struct used to deserialize CSV data into ExpenseRecord structs
 #[derive(Debug, Deserialize)]
 pub struct IntermediateExpenseRecord {
     date: String,
@@ -12,6 +13,7 @@ pub struct IntermediateExpenseRecord {
     amount: f64,
 }
 
+/// Main struct used to represent the individual expense data
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct ExpenseRecord {
     date: NaiveDate,
@@ -26,25 +28,23 @@ impl ExpenseRecord {
             amount,
         }
     }
-
+    /// Converts an IntermediateExpenseRecord into an ExpenseRecord
     pub fn from_intermediate_expense_record(
         intermediate_record: IntermediateExpenseRecord,
     ) -> ExpenseRecord {
         // date,category,amount,
         let date_str = &intermediate_record.date;
 
-        let mut date_numbers: Vec<u32> = Vec::new();
+        let mut date_numbers: Vec<&str> = Vec::new();
         for num in date_str.split('/') {
-            date_numbers.push(num.parse::<u32>().unwrap());
+            date_numbers.push(num);
         }
 
-        let day = date_numbers.get(0).unwrap();
-        let month = date_numbers.get(1).unwrap();
-        let year = date_numbers.get(2).unwrap();
+        let day = date_numbers.get(0).unwrap().parse::<u32>().unwrap();
+        let month = date_numbers.get(1).unwrap().parse::<u32>().unwrap();
+        let year = date_numbers.get(2).unwrap().parse::<i32>().unwrap();
 
-        let year_i32: i32 = *year as i32;
-        println!("{year}, {month}, {day}");
-        let date_obj = NaiveDate::from_ymd_opt(year_i32, *month, *day).unwrap();
+        let date_obj = NaiveDate::from_ymd_opt(year, month, day).unwrap();
 
         ExpenseRecord::new(
             date_obj,
@@ -54,20 +54,14 @@ impl ExpenseRecord {
     }
 }
 
-#[tauri::command]
 pub fn convert_csv_to_expense_record(
     csv_str: String,
 ) -> Result<Vec<ExpenseRecord>, Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(csv_str.as_bytes());
-    let mut intermediate_records = Vec::new();
+    let mut expense_records = Vec::new();
     for result in rdr.deserialize() {
         let record: IntermediateExpenseRecord = result?;
-        intermediate_records.push(record);
-    }
-
-    let mut expense_records: Vec<ExpenseRecord> = Vec::new();
-    for record in intermediate_records.into_iter() {
-        expense_records.push(ExpenseRecord::from_intermediate_expense_record(record))
+        expense_records.push(ExpenseRecord::from_intermediate_expense_record(record));
     }
 
     Ok(expense_records)
@@ -94,10 +88,6 @@ mod tests {
                 amount: 2.0,
             },
         ];
-        println!(
-            "[MARKER] {:?}",
-            convert_csv_to_expense_record(csv_str.to_string())
-        );
 
         assert_eq!(
             convert_csv_to_expense_record(csv_str.to_string()).unwrap(),
