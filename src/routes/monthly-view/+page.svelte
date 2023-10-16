62db8a7906abb89ac7440196af0fa136967f2d3f
Darric Heng <darricheng@gmail.com>
2023-10-16 21:34:26 +0800

    initial chartjs setup with dummy data

--- src/routes/monthly-view/+page.svelte
+++ src/routes/monthly-view/+page.svelte
@@ -1,3 +1,29 @@
+<script lang="ts">
+  import Chart from 'chart.js/auto';
+  import { onMount } from 'svelte';
+
+  let chartValues = [20, 10, 5, 2, 20, 30, 45];
+  let chartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  let chartCanvas: HTMLCanvasElement;
+
+  onMount(async () => {
+    new Chart(chartCanvas, {
+      type: 'bar',
+      data: {
+        labels: chartLabels,
+        datasets: [
+          {
+            label: 'Revenue',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: chartValues,
+          },
+        ],
+      },
+    });
+  });
+</script>
+
 <div class="grid grid-cols-4 h-full">
   <div class="p-4 flex flex-col gap-4 items-center h-full">
     <h1 class="h1 text-center">Monthly View</h1>
@@ -21,6 +47,8 @@
     </div>
   </div>
   <div class="p-4 col-span-3">
-    <div class="border-white border-2 rounded-lg h-full w-full">Chart here</div>
+    <div class="border-white border-2 rounded-lg h-full w-full">
+      <canvas bind:this={chartCanvas} id="monthly-view-chart"></canvas>
+    </div>
   </div>
 </div>
