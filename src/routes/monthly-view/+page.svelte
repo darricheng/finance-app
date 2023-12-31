59a9cfac9805498c0276541b0506cebfec5afcfa
Darric Heng <darricheng@gmail.com>
2023-12-31 13:08:52 +0800

    update chart to render selected month data

--- src/routes/monthly-view/+page.svelte
+++ src/routes/monthly-view/+page.svelte
@@ -9,39 +9,37 @@
     amount: number;
   }[];
 
-  type Dates = {
+  type Date = {
     month: number;
     year: number;
-  }[];
+  };
+  type Dates = Date[];
 
   // Element bindings
   let chartCanvas: HTMLCanvasElement;
+  let selectedDateIndex = 0;
 
   // Dates variable definitions
   let dates: Dates = [];
 
+  let chart: Chart;
+
   // Chart data variable definitions
   let budgetBreakdown: ChartData = [];
-  let expensesData: ChartData = [
-    { category: 'Food', amount: 111.9 },
-    { category: 'Transport', amount: 111.9 },
-    { category: 'Category X', amount: 333.9 },
-    { category: 'Category Y', amount: 111.9 },
-    { category: 'Category Z', amount: 333.9 },
-  ];
-  let chartColors: string[] = [];
-
-  // TODO: Probably have to shift this into updateData() once I get the data from the backend?
-  let chartValues = expensesData.map((data) => data.amount);
-  let chartLabels = expensesData.map((data) => data.category);
+  let expensesData: ChartData = [];
 
   // Update the chart data
-  function updateData(budget: ChartData, expenses: ChartData) {
-    budgetBreakdown = budget;
-    // NOTE: This is commented out because the backend does not return the expenses data yet
-    // expensesData = expenses;
+  async function updateData() {
+    // Get chart data
+    const { budget, expenses }: { budget: ChartData; expenses: ChartData } = await invoke(
+      'get_monthly_chart_data',
+      { month: dates[selectedDateIndex].month, year: dates[selectedDateIndex].year }
+    );
 
-    chartColors = expensesData.map((data) => {
+    budgetBreakdown = budget;
+    expensesData = expenses;
+
+    chart.data.datasets[0].backgroundColor = expensesData.map((data) => {
       const amountSpent = data.amount;
       const budgetedAmount = budgetBreakdown.find(
         (element) => element.category === data.category
@@ -50,25 +48,20 @@
       if (amountSpent <= budgetedAmount) return 'green'; // within budget!
       else return 'red'; // spent too much
     });
+    chart.data.datasets[0].data = expensesData.map((data) => data.amount);
+    chart.data.labels = expensesData.map((data) => data.category);
+    chart.update();
   }
 
   onMount(async () => {
     // Get the dates
     dates = await invoke('get_dates');
     const earliestDate = dates[0];
-
-    // Get chart data
-    const data: {
-      budget: ChartData;
-      expenses: ChartData;
-    } = await invoke('get_monthly_chart_data', {
-      month: earliestDate.month,
-      year: earliestDate.year,
-    });
-    updateData(data.budget, data.expenses);
+    dates[selectedDateIndex].month = earliestDate.month;
+    dates[selectedDateIndex].year = earliestDate.year;
 
     // Render the chart
-    new Chart(chartCanvas, {
+    chart = new Chart(chartCanvas, {
       type: 'bar',
       plugins: [ChartDataLabels], // plugin to show values on the bars
       options: {
@@ -87,27 +80,29 @@
         },
       },
       data: {
-        labels: chartLabels,
+        labels: [],
         datasets: [
           {
-            backgroundColor: chartColors,
-            data: chartValues,
+            backgroundColor: [],
+            data: [],
           },
         ],
       },
     });
+
+    updateData();
   });
 </script>
 
 <div class="grid grid-cols-4 h-full">
   <div class="p-4 flex flex-col gap-4 items-center h-full">
     <h1 class="h1 text-center">Monthly View</h1>
-    <select class="select">
+    <select class="select" bind:value={selectedDateIndex} on:change={updateData}>
       {#if dates.length === 0}
-        <option>NO DATA</option>
+        <option value={0}>NO DATA</option>
       {/if}
-      {#each dates as date}
-        <option value={date.month}>{date.year} - {date.month}</option>
+      {#each dates as date, index}
+        <option value={index}>{date.year} - {date.month}</option>
       {/each}
     </select>
     <div class="border-white border-2 rounded-lg p-4 w-full grow">
