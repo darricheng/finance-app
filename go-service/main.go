25ed1cfbb510c6f190e43f22d3a3605c45899fd7
Darric Heng <darricheng@gmail.com>
2024-01-12 20:40:04 +0800

    add graceful shutdown

--- go-service/main.go
+++ go-service/main.go
@@ -1,14 +1,51 @@
 package main
 
 import (
+	"context"
 	"finance-app-service/routes"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+/*
+See the [graceful shutdown example](https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/notify-with-context/server.go)
+for the referred code to shutdown the server gracefully
+*/
+
 func main() {
-	router := gin.Default()
-	v1 := router.Group("/v1")
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	r := gin.Default()
+	v1 := r.Group("/v1")
 	routes.AddRoutes(v1)
-	router.Run()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
