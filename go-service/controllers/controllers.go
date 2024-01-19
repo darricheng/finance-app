f10f115a6a5b2956c66db61e6f78cebab6073c44
Darric Heng <darricheng@gmail.com>
2024-01-19 21:00:17 +0800

    use gorm datatypes for validation instead

--- go-service/controllers/controllers.go
+++ go-service/controllers/controllers.go
@@ -2,8 +2,6 @@ package controllers
 
 import (
 	"net/http"
-	"strings"
-	"time"
 
 	"finance-app-service/models"
 	"finance-app-service/repository"
@@ -20,19 +18,6 @@ func HandleNewEntry(c *gin.Context) {
 	var json models.FinanceEntry
 	if err := c.ShouldBindJSON(&json); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	var validationErrors []string
-
-	entryDate := json.Date
-	_, dateErr := time.Parse(time.DateOnly, entryDate)
-	if dateErr != nil {
-		validationErrors = append(validationErrors, dateErr.Error())
-	}
-
-	if len(validationErrors) > 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": strings.Join(validationErrors, "; ")})
 		return
 	}
 
--- go-service/models/models.go
+++ go-service/models/models.go
@@ -1,13 +1,17 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/datatypes"
+)
 
 type FinanceEntry struct {
 	// ISO 8601 date format, i.e. YYYY-MM-DD
-	Date        string  `json:"date" binding:"required"`
-	Category    string  `json:"category" binding:"required"`
-	Amount      float64 `json:"amount" binding:"required"`
-	Description string  `json:"description" binding:"required"`
+	Date        datatypes.Date `json:"date" binding:"required"`
+	Category    string         `json:"category" binding:"required"`
+	Amount      float64        `json:"amount" binding:"required"`
+	Description string         `json:"description" binding:"required"`
 	// DB only fields
 	ID        uint
 	CreatedAt time.Time
